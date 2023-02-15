841624aa5adde373109b9ce833e936b08e313ba5
diobergue <vianadiobergue@gmail.com>
2023-02-15 10:36:49 -0300

    pasta01
    pasta1

---
+++ estudos01/README.md
@@ -0,0 +1,4 @@
+# ESTUDOS-
+ HTML e CSS
+
+atualizando ...
---
+++ estudos01/estudos de hmtl e css/java.js
@@ -0,0 +1,10 @@
+
+let corSite = 'azul';
+
+function resetaCor(cor,tom){
+    corSite = cor + tom;
+};
+
+console.log(corSite)
+resetaCor('vermelho',' claro');
+console.log(corSite);
